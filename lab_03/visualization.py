84dd9d7742f1e0c68934796dc4db9cf74a33d469
Balthazar Neveu <balthazarneveu@gmail.com>
2023-11-22 15:18:13 +0100

    visualization of signals

--- lab_03/visualization.py
+++ lab_03/visualization.py
@@ -1,15 +1,31 @@
 
 import matplotlib.pyplot as plt
-import numpy as np
-from typing import Dict
 
 
-def visualize_signals(data_in=None,
+def visualize_signals(data_in: list = None,
                       idx: int = 0):
+    """Visualize signals, better used interactively with ipywidget
+
+    Args:
+        data_in (list): data structure as a list 
+        [signals, snr, labels_id, label_dict]
+        idx (int, optional): Specific index of the signal to plot.
+        Defaults to 0.
+
+    Usage:
+    ======
+    Use with ipywidget interactive like in a notebook
+    ```
+    from ipywidgets import interact, IntSlider, fixed
+    interact(
+        visualize_signals,
+        data_in = fixed([signals, snr, labels_id, label_dict]),
+        idx=IntSlider(min=0, max=signals.shape[0]-1, step=1)
+    )
+    ```
+    """
     signals, snr, labels_id, label_dict = data_in
     plt.figure(figsize=(8, 8))
-    # plt.plot(signals[idx, :, 0], label="real")
-    # plt.plot(signals[idx, :, 1], label="imaginary")
     plt.scatter(signals[idx, :, 0], signals[idx, :, 1],
                 label="complex", alpha=0.5)
     plt.title(f'Index {idx} - {label_dict.get(labels_id[idx], "unknown")}'
