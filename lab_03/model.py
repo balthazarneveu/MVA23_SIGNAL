a96e210f5321fdecea60ec54bb82ae4fda1b7e32
Balthazar Neveu <balthazarneveu@gmail.com>
2023-11-29 20:52:44 +0100

    model 5

--- lab_03/model.py
+++ lab_03/model.py
@@ -52,10 +52,12 @@ class VanillaClassifier(torch.nn.Module):
         return logits
 
 
-class Exp_baseline(torch.nn.Module):
+class Convolutional_baseline(torch.nn.Module):
     def __init__(self, ch_in: int = 2, n_classes: int = N_CLASSES,
                  h_dim=8, h_dim_classifier=128, k_size=5,
-                 pool_temporal=8
+                 pool_temporal=8,
+                 rnn=False,
+                 h_dim_rnn = 32
                  ) -> None:
         super().__init__()
         self.conv1 = torch.nn.Conv1d(
@@ -72,9 +74,22 @@ class Exp_baseline(torch.nn.Module):
             self.relu,
             self.pool
         )
-        self.pool_temporal = torch.nn.AvgPool1d(kernel_size=pool_temporal)
-        flat_dim = h_dim*2048//4//4 // pool_temporal
-        self.fc1 = torch.nn.Linear(flat_dim, h_dim_classifier)
+        self.rnn = rnn
+        conv_length_output = 2048//4//4
+        if rnn:
+            
+            self.gru = torch.nn.GRU(
+                h_dim,
+                h_dim_rnn,
+                num_layers=2,
+                batch_first=True,
+                bidirectional=True
+            )
+            classif_dim  = h_dim_rnn*2
+        else:
+            self.pool_temporal = torch.nn.AvgPool1d(kernel_size=pool_temporal)
+            classif_dim = h_dim*conv_length_output // pool_temporal
+        self.fc1 = torch.nn.Linear(classif_dim, h_dim_classifier)
         self.fc2 = torch.nn.Linear(h_dim_classifier, n_classes)
         self.classifier = torch.nn.Sequential(
             self.fc1,
@@ -96,8 +111,13 @@ class Exp_baseline(torch.nn.Module):
         features = self.feature_extractor(sig_in)
         # Global pooling
         # [N, h, ?] -> [N, h]
-        vector = self.pool_temporal(features)
-        vector = vector.view(-1, vector.shape[-1]*vector.shape[-2])
+        if self.rnn:
+            features = features.transpose(1, 2)
+            full_hidden_states, _ = self.gru(features)
+            vector = full_hidden_states[:, -1, :]
+        else:
+            vector = self.pool_temporal(features)
+            vector = vector.view(-1, vector.shape[-1]*vector.shape[-2])
         # Vector classifier
         # [N, 64] -> [N, n_classes]
         logits = self.classifier(vector)
@@ -122,7 +142,8 @@ class RnnBaseline(torch.nn.Module):
             batch_first=True,
             bidirectional=bidirectional
         )
-        self.fc1 = torch.nn.Linear(h_dim, h_dim_classifier)
+        self.fc1 = torch.nn.Linear(
+            h_dim*(2 if bidirectional else 1), h_dim_classifier)
         self.fc2 = torch.nn.Linear(h_dim_classifier, n_classes)
         self.relu = torch.nn.ReLU()
         self.classifier = torch.nn.Sequential(
@@ -132,20 +153,17 @@ class RnnBaseline(torch.nn.Module):
         )
 
     def forward(self, sig_in: torch.Tensor) -> torch.Tensor:
-        """Perform recrurent feature extraction followed by classifier head
+        """Perform recurrent feature extraction followed by classifier head
 
         Args:
-            sig_in (torch.Tensor): [N, C, T]
+            sig_in (torch.Tensor): [N, C, T] -> need to transpose first
 
         Returns:
             torch.Tensor: logits (not probabilities) [N, n_classes]
         """
-        # print(sig_in.shape)
         sig_in = sig_in.transpose(1, 2)
-        # print(sig_in.shape)
-        full, _ = self.gru(sig_in)
-        hidden_features = full[:, -1, :]
-        # print(hidden_features.shape)
+        full_hidden_states, _ = self.gru(sig_in)
+        hidden_features = full_hidden_states[:, -1, :]
         # Vector classifier
         logits = self.classifier(hidden_features)
         return logits
@@ -160,14 +178,23 @@ def get_experience(exp):
     if exp == 0:
         model = VanillaClassifier()  # 40%
     elif exp == 1:
-        model = Exp_baseline(pool_temporal=8)  # 49%
+        model = Convolutional_baseline(pool_temporal=8)  # 49%
     elif exp == 2:
-        model = Exp_baseline(pool_temporal=4)  # 61%
+        model = Convolutional_baseline(pool_temporal=4)  # 61%
         hyperparams = dict(
             lr=1E-4,
             n_epochs=300,
             batch_sizes=(256, 128)
         )
     elif exp == 3:
-        model = RnnBaseline(bidirectional=False)
+        model = RnnBaseline(bidirectional=False, h_dim=8)  # 20% h_dim=8
+    elif exp == 4:
+        model = RnnBaseline(bidirectional=True, h_dim=16)
+    elif exp == 5:
+        model = Convolutional_baseline(rnn=True)  # 61%
+        hyperparams = dict(
+            lr=1E-4,
+            n_epochs=500,
+            batch_sizes=(256, 128)
+        )
     return model, hyperparams
