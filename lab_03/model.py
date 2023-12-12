48c8939412c355cc2a95f05a71ed11ffda617321
MathildeDupouy <82039222+MathildeDupouy@users.noreply.github.com>
2023-12-12 17:59:42 +0100

    archi big_convolutional

--- lab_03/model.py
+++ lab_03/model.py
@@ -210,6 +210,89 @@ class Slim_Convolutional(torch.nn.Module):
         logits = self.classifier(vector)
         return logits
 
+class Big_Convolutional(torch.nn.Module):
+    def __init__(self, ch_in: int = 2,
+                 n_classes: int = N_CLASSES,
+                 h_dim=16,
+                 h_dim_classifier=128,
+                 k_size=5,
+                 ) -> None:
+        super().__init__()
+        self.conv1 = torch.nn.Conv1d(
+            ch_in, h_dim, k_size, padding=k_size//2)
+        self.conv2 = torch.nn.Conv1d(
+            h_dim, h_dim, k_size, padding=k_size//2)
+        self.conv3 = torch.nn.Conv1d(
+            h_dim, h_dim, k_size, padding=k_size//2)
+        self.conv4 = torch.nn.Conv1d(
+            h_dim, h_dim, k_size, padding=k_size//2)
+        self.conv5 = torch.nn.Conv1d(
+            h_dim, h_dim, k_size, padding=k_size//2)
+        self.pool = torch.nn.MaxPool1d(kernel_size=2)
+        self.relu = torch.nn.ReLU()
+        self.feature_extractor = torch.nn.Sequential(
+            self.conv1,
+            self.relu,
+            self.pool,
+            self.conv2,
+            self.relu,
+            self.pool,
+            self.conv3,
+            self.relu,
+            self.pool,
+            self.conv4,
+            self.relu,
+            self.pool,
+            self.conv5,
+            self.relu,
+            self.pool
+        )
+        conv_length_output = 2048//4//4//4
+        self.conv1_1 = torch.nn.Conv1d(
+            h_dim, h_dim, kernel_size = 1)
+        self.relu = torch.nn.ReLU()
+        self.conv1_2 = torch.nn.Conv1d(
+            h_dim, h_dim, kernel_size = 1)
+        self.conv1_3 = torch.nn.Conv1d(
+            h_dim, h_dim, kernel_size = 1)
+        self.final_maxpool = torch.nn.MaxPool1d(conv_length_output)
+        self.feature_for_classif = torch.nn.Sequential(
+            self.conv1_1,
+            self.relu,
+            self.conv1_2,
+            self.relu,
+            self.conv1_3,
+            self.relu,
+            self.final_maxpool
+        )
+        self.fc1 = torch.nn.Linear(h_dim, h_dim_classifier)
+        self.fc2 = torch.nn.Linear(h_dim_classifier, n_classes)
+        self.classifier = torch.nn.Sequential(
+            self.fc1,
+            self.relu,
+            self.fc2
+        )
+    
+    def forward(self, sig_in: torch.Tensor) -> torch.Tensor:
+        """Perform feature extraction followed by classifier head
+
+        Args:
+            sig_in (torch.Tensor): [N, C, T]
+
+        Returns:
+            torch.Tensor: logits (not probabilities) [N, n_classes]
+        """
+        # Convolution backbone
+        # [N, C, T] -> [N, h, T//64]
+        features = self.feature_extractor(sig_in)
+        # Global reordering and pooling
+        # [N, h, T//64] -> [N, h]
+        features_out = self.feature_for_classif(features)
+
+        # Vector classifier
+        # [N, h] -> [N, n_classes]
+        logits = self.classifier(features_out)
+        return logits
 
 class RnnBaseline(torch.nn.Module):
     def __init__(
@@ -334,16 +417,30 @@ def get_experience(exp):
         hyperparams["lr"] = 1e-3  # default to 1E-4
     elif exp == 21:
         model = Slim_Convolutional(rnn=False)
-        hyperparams["n_epochs"] = 10
+        hyperparams["n_epochs"] = 300
         hyperparams["lr_scheduler_name"] = "ExponentialLR_0.9"
         hyperparams["lr_scheduler"] = lambda optimizer: torch.optim.lr_scheduler.ExponentialLR(
             optimizer, gamma=0.9)
     elif exp == 22:
         model = Slim_Convolutional(rnn=False)
-        hyperparams["n_epochs"] = 10
+        hyperparams["n_epochs"] = 300
         hyperparams["lr_scheduler_name"] = "Plateau_0.1_2_1e-4"
         hyperparams["lr_scheduler"] = lambda optimizer: torch.optim.lr_scheduler.ReduceLROnPlateau(
             optimizer, factor=0.1, patience=2, threshold=1e-4)
         hyperparams["needed_loss_scheduler"] = True
-
+    # Bigger Convolutional experiments
+    elif exp == 23 :
+        model = Slim_Convolutional(rnn=False, h_dim=64)
+        hyperparams["n_epochs"] = 300
+        hyperparams["lr_scheduler_name"] = "Plateau_0.1_2_1e-4"
+        hyperparams["lr_scheduler"] = lambda optimizer: torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer, factor=0.1, patience=2, threshold=1e-4)
+        hyperparams["needed_loss_scheduler"] = True
+    elif exp == 24 :
+        model = Big_Convolutional(h_dim=64)
+        hyperparams["n_epochs"] = 300
+        hyperparams["lr_scheduler_name"] = "Plateau_0.1_2_1e-4"
+        hyperparams["lr_scheduler"] = lambda optimizer: torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer, factor=0.1, patience=2, threshold=1e-4)
+        hyperparams["needed_loss_scheduler"] = True
     return model, hyperparams, augment_config
